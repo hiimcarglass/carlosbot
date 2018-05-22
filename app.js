9dc2d01684a67e57dfcbc1bdedc1e65874df2b4a
LAPTOP-N9U2HJQN <carlossevilla112@gmail.com>
2018-05-22 17:03:16 +0200

    Segundo y ultimo push + codigo acabado y funcionando

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const art = require('ascii-art');
 
 client.on('ready', () => {
+
   console.log(`Logged in as ${client.user.tag}!`);
+
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+
+  if (msg.content === '!ascii') {
+
+    art.font ("Demo!","Doom",function(rendered){
+
+      console.log(rendered);
+      msg.channel.send(rendered);
+
+    })
+
   }
+
 });
 
 client.login('NDMzNzU0MzM4MzQ3OTc0NjU2.DeW7OA.qikYv8sGm2rMF8Vm9k5TN_OshL4');
